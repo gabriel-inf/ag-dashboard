8fd7dcbbb30b26cd716531bb5995ea6910ac910e
Gabriel Stepien <gabriel.stepien.eu@gmail.com>
2019-07-23 01:26:35 -0300

    Adding logo

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">Nuestros <b>valores</b> nos identifican, diferencian de los demás y retan a ser mejores.<b><a href="https://www.corporacionag.com/" target="_blank"> website</a></b></span>
+  `,
+})
+export class FooterComponent {
+}
